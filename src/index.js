b3c1e04009adbbc420705cf9fc10426498dab531
Kate Schumacher <kate.schumacher@gmail.com>
2024-12-11 10:52:37 -0600

    Add styling to nav and footer

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ const textWrapper = document.createElement("div");
 textWrapper.classList.add("text-wrapper");
 content.appendChild(textWrapper);
 
+const footer = document.createElement("div");
+footer.classList.add("footer");
+footer.textContent = "Made by Kate Schumacher";
+content.appendChild(footer);
+
 addItem("small", textWrapper, "You can eat them.");
 addItem("big", textWrapper, "Eat Sanrio Characters.");
-addItem("footer", content, "Made by Kate Schumacher");
+addItem("black-bar", footer, "");
 
 function addItem(classToAdd, appendTo, text, type="div") {
     const newElement = document.createElement(type);
